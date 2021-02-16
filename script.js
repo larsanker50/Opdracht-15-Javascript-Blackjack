eae2fd8346b7072e653948d939b7b643bd2456c4
larsanker50 <75078152+larsanker50@users.noreply.github.com>
2021-02-16 15:58:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-const playerCards = document.getElementById("playerCards")
-const dealerCards = document.getElementById("dealerCards")
-const score = document.getElementById("score")
-const delenButton = document.getElementById("delenButton")
-const pasButton = document.getElementById("pasButton")
-const card = document.getElementsByClassName("card")
-let playerCardsArray = []
+const playerCards = document.getElementById("playerCards");
+const dealerCards = document.getElementById("dealerCards");
+const score = document.getElementById("score");
+const delenButton = document.getElementById("delenButton");
+const pasButton = document.getElementById("pasButton");
+const playerCard = document.getElementsByClassName("playerCard");
+const dealerCard = document.getElementsByClassName("dealerCard");
+let playerCardsArray = [];
+let dealerCardsArray = [];
+let usedCardsArray = [];
+let delenFunctionUsed = false;
+let amountPlayerCards = 0
+let amountDealerCards = 0
+
+//dit is de delen knop functie
+delenButton.onclick = function() {
+  if (delenFunctionUsed == false) {
+    delenFunction();
+  } else {
+    hitFunction();
+  };
+};
+
 
 //dit stukje code maakt de kaart objecten aan in de Deck class
 class Deck {
@@ -24,10 +40,10 @@ let card6 = new Deck(7, "heart", 7);
 let card7 = new Deck(8, "heart", 8);
 let card8 = new Deck(9, "heart", 9)
 let card9 = new Deck(10, "heart", 10)
-let card10 = new Deck(10, "heart", "Jack")
-let card11 = new Deck(10, "heart", "Queen")
-let card12 = new Deck(10, "heart", "King")
-let card13 = new Deck(11, "heart", "Ace")
+let card10 = new Deck(10, "heart", "jack")
+let card11 = new Deck(10, "heart", "queen")
+let card12 = new Deck(10, "heart", "king")
+let card13 = new Deck(11, "heart", "ace")
 let card14 = new Deck(2, "club", 2)
 let card15 = new Deck(3, "club", 3)
 let card16 = new Deck(4, "club", 4)
@@ -37,10 +53,10 @@ let card19 = new Deck(7, "club", 7)
 let card20 = new Deck(8, "club", 8)
 let card21 = new Deck(9, "club", 9)
 let card22 = new Deck(10, "club", 10)
-let card23 = new Deck(10, "club", "Jack")
-let card24 = new Deck(10, "club", "Queen")
-let card25 = new Deck(10, "club", "King")
-let card26 = new Deck(11, "club", "Ace")
+let card23 = new Deck(10, "club", "jack")
+let card24 = new Deck(10, "club", "queen")
+let card25 = new Deck(10, "club", "king")
+let card26 = new Deck(11, "club", "ace")
 let card27 = new Deck(2, "diamond", 2)
 let card28 = new Deck(3, "diamond", 3)
 let card29 = new Deck(4, "diamond", 4)
@@ -50,10 +66,10 @@ let card32 = new Deck(7, "diamond", 7)
 let card33 = new Deck(8, "diamond", 8)
 let card34 = new Deck(9, "diamond", 9)
 let card35 = new Deck(10, "diamond", 10)
-let card36 = new Deck(10, "diamond", "Jack")
-let card37 = new Deck(10, "diamond", "Queen")
-let card38 = new Deck(10, "diamond", "King")
-let card39 = new Deck(11, "diamond", "Ace")
+let card36 = new Deck(10, "diamond", "jack")
+let card37 = new Deck(10, "diamond", "queen")
+let card38 = new Deck(10, "diamond", "king")
+let card39 = new Deck(11, "diamond", "ace")
 let card40 = new Deck(2, "spade", 2)
 let card41 = new Deck(3, "spade", 3)
 let card42 = new Deck(4, "spade", 4)
@@ -63,16 +79,70 @@ let card45 = new Deck(7, "spade", 7)
 let card46 = new Deck(8, "spade", 8)
 let card47 = new Deck(9, "spade", 9)
 let card48 = new Deck(10, "spade", 10)
-let card49 = new Deck(10, "spade", "Jack")
-let card50 = new Deck(10, "spade", "Queen")
-let card51 = new Deck(10, "spade", "King")
-let card52 = new Deck(11, "spade", "Ace")
+let card49 = new Deck(10, "spade", "jack")
+let card50 = new Deck(10, "spade", "queen")
+let card51 = new Deck(10, "spade", "king")
+let card52 = new Deck(11, "spade", "ace")
 
 //dit stukje code pakt een willekeurige kaart
-
-function randomCard() {
+function randomPlayerCard() {
         let random = Math.floor((Math.random() * 52) + 1);
         let card = ("card" + random)
-        playerCardsArray.push(eval(card));
-        console.log(card)
-    }
+        if (usedCardsArray.includes(card.toString()) == false) {
+          playerCardsArray.push(eval(card));
+          usedCardsArray.push(card.toString());
+          //dit stukje code maakt nieuwe HTML kaarten aan.
+          let div = playerCards.appendChild(document.createElement("div"));
+          div.classList.add("playerCard")
+          playerCard[amountPlayerCards].innerHTML = ((playerCardsArray[amountPlayerCards].symbol)+",\n"+(playerCardsArray[amountPlayerCards].rank))
+          playerCard[amountPlayerCards].classList.add(playerCardsArray[amountPlayerCards].symbol)
+          console.log("player" + card);
+          console.log(playerCardsArray)
+          amountPlayerCards++
+        } else if (usedCardsArray.length < 52){
+          randomPlayerCard()
+        } else {
+          alert("De kaarten zijn op")
+        };
+  };
+
+function randomDealerCard() {
+        let random = Math.floor((Math.random() * 52) + 1);
+        let card = ("card" + random)
+        if (usedCardsArray.includes(card.toString()) == false) {
+          dealerCardsArray.push(eval(card));
+          usedCardsArray.push(card.toString());
+          //dit stukje code maakt nieuwe HTML kaarten aan.
+          let div = dealerCards.appendChild(document.createElement("div"))
+          div.classList.add("dealerCard")
+          if (amountDealerCards > 0) {
+            dealerCard[amountDealerCards].innerHTML = ((dealerCardsArray[amountDealerCards].symbol)+",\n"+(dealerCardsArray[amountDealerCards].rank))
+            dealerCard[amountDealerCards].classList.add(dealerCardsArray[amountDealerCards].symbol)
+          } else {
+            dealerCard[amountDealerCards].innerHTML = "secret"+"\n"+"secret";
+            dealerCard[amountDealerCards].classList.add("brown")
+          }
+          console.log("dealer" + card);
+          console.log(dealerCardsArray)
+          amountDealerCards++
+        } else if (usedCardsArray.length < 52){
+          randomDealerCard()
+        } else {
+          alert("De kaarten zijn op")
+        }
+  };  
+
+  // dit is de functie voor de delenfunctieknop
+function delenFunction() {
+  randomPlayerCard()
+  randomPlayerCard()
+  randomDealerCard()
+  randomDealerCard()
+  delenFunctionUsed = true
+  delenButton.innerHTML = "Hit";
+}
+
+// dit is de functie voor de hitfunctieknop
+function hitFunction() {
+
+}
