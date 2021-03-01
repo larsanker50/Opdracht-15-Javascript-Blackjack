81ad1b8eca231696d965ab93fee13b75cc8e72a9
Lars Anker <75078152+larsanker50@users.noreply.github.com>
2021-03-01 15:01:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,73 +21,45 @@ let delenFunctionUsed = false;
 let gameEnd = false;
 
 //dit stukje code maakt de kaart objecten aan in de Deck class              //loop maken uit de class halen. objecten.
-class Deck {
-  constructor(value, symbol, rank) {
-    this.value = value;
-    this.symbol = symbol;
-    this.rank = rank;
-  };
-};
+cards = []
+let card = ""
+cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+cardRanks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"]
+cardSymbols = ["heart", "club", "diamond", "spade"]
 
-let card1 = new Deck(2, "heart", 2);
-let card2 = new Deck(3, "heart", 3);
-let card3 = new Deck(4, "heart", 4);
-let card4 = new Deck(5, "heart", 5);
-let card5 = new Deck(6, "heart", 6);
-let card6 = new Deck(7, "heart", 7);
-let card7 = new Deck(8, "heart", 8);
-let card8 = new Deck(9, "heart", 9);
-let card9 = new Deck(10, "heart", 10);
-let card10 = new Deck(10, "heart", "jack");
-let card11 = new Deck(10, "heart", "queen");
-let card12 = new Deck(10, "heart", "king");
-let card13 = new Deck(11, "heart", "ace");
-let card14 = new Deck(2, "club", 2);
-let card15 = new Deck(3, "club", 3);
-let card16 = new Deck(4, "club", 4);
-let card17 = new Deck(5, "club", 5);
-let card18 = new Deck(6, "club", 6);
-let card19 = new Deck(7, "club", 7);
-let card20 = new Deck(8, "club", 8);
-let card21 = new Deck(9, "club", 9);
-let card22 = new Deck(10, "club", 10);
-let card23 = new Deck(10, "club", "jack");
-let card24 = new Deck(10, "club", "queen");
-let card25 = new Deck(10, "club", "king");
-let card26 = new Deck(11, "club", "ace");
-let card27 = new Deck(2, "diamond", 2);
-let card28 = new Deck(3, "diamond", 3);
-let card29 = new Deck(4, "diamond", 4);
-let card30 = new Deck(5, "diamond", 5);
-let card31 = new Deck(6, "diamond", 6);
-let card32 = new Deck(7, "diamond", 7);
-let card33 = new Deck(8, "diamond", 8);
-let card34 = new Deck(9, "diamond", 9);
-let card35 = new Deck(10, "diamond", 10);
-let card36 = new Deck(10, "diamond", "jack");
-let card37 = new Deck(10, "diamond", "queen");
-let card38 = new Deck(10, "diamond", "king");
-let card39 = new Deck(11, "diamond", "ace");
-let card40 = new Deck(2, "spade", 2);
-let card41 = new Deck(3, "spade", 3);
-let card42 = new Deck(4, "spade", 4);
-let card43 = new Deck(5, "spade", 5);
-let card44 = new Deck(6, "spade", 6);
-let card45 = new Deck(7, "spade", 7);
-let card46 = new Deck(8, "spade", 8);
-let card47 = new Deck(9, "spade", 9);
-let card48 = new Deck(10, "spade", 10);
-let card49 = new Deck(10, "spade", "jack");
-let card50 = new Deck(10, "spade", "queen");
-let card51 = new Deck(10, "spade", "king");
-let card52 = new Deck(11, "spade", "ace");
+
+//dit stukje code maakt de kaartobjecten aan.
+let value = ""
+let rank = ""
+let symbol = ""
+
+for (cardNumber = 0; cardNumber <= 51; cardNumber++) {
+  if (cards.length < 52) {
+  for (let i = 0; i < cardSymbols.length; i++){
+    for (let x = 0; x < cardRanks.length; x++) {
+      cardValue = cardValues[x]
+      cardRank = cardRanks[x]
+      cardSymbol = cardSymbols[i]
+      window["card" + (cardNumber + 1)] = {
+        value: cardValue,
+        rank: cardRank,
+        symbol: cardSymbol
+      }
+      cards.push(eval("card" + (cardNumber + 1)))
+    }
+  }
+}
+  for (f = 0; f < 52; f++)
+  window["card" + (f + 1)] = cards[f]
+}
+
 
 //dit stukje code pakt een willekeurige kaart voor de speler              //in verschillende functies zetten.
 function randomPlayerCard() {
-  let random = Math.floor((Math.random() * 52) + 1);
-  let card = ("card" + random);
+  let random = Math.floor((Math.random() * 51));
+  let card = (cards[random]);
   if (usedCardsArray.includes(card.toString()) == false) {
-    playerCardsArray.push(eval(card));
+    playerCardsArray.push(card);
     usedCardsArray.push(card.toString());
     //dit stukje code maakt nieuwe HTML kaarten aan.
     let div = playerCards.appendChild(document.createElement("div"));
@@ -122,10 +94,10 @@ function randomPlayerCard() {
 };
 //dit stukje code pakt een willekeurige kaart voor de dealer              //in verschillende functies zetten. kijken welke functies dubbel zijn.
 function randomDealerCard() {
-  let random = Math.floor((Math.random() * 52) + 1);
-  let card = ("card" + random);
+  let random = Math.floor((Math.random() * 51));
+  let card = (cards[random]);
   if (usedCardsArray.includes(card.toString()) == false) {
-    dealerCardsArray.push(eval(card));
+    dealerCardsArray.push(card);
     usedCardsArray.push(card.toString());
     //dit stukje code maakt nieuwe HTML kaarten aan.
     let div = dealerCards.appendChild(document.createElement("div"));
